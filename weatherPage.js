001ecfb049b8f0a77063d4b35132257f66caca7d
armstrongAJA <157639160+armstrongAJA@users.noreply.github.com>
2025-08-07 20:59:19 +0100

    Update weatherPage.js

--- weatherPage.js
+++ weatherPage.js
@@ -36,7 +36,8 @@ function pictocodeToFilename(code) {
   return `${codeStr}_iday.svg`;
 }
   function buildApiUrl() {
-    return `https://my.meteoblue.com/packages/basic-1h_basic-day?lat=${LAT}&lon=${LON}&asl=35&tz=Europe/London&name=${LOCATION}&format=json&apikey=${API_KEY}`;
+    return `https://weatherapp-3o2e.onrender.com/weather?lat=lat=${LAT}&lon=${LON}`
+    //return `https://my.meteoblue.com/packages/basic-1h_basic-day?lat=${LAT}&lon=${LON}&asl=35&tz=Europe/London&name=${LOCATION}&format=json&apikey=${API_KEY}`;
   }
 
   async function fetchWeatherData() {
